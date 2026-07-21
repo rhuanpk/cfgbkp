e2294ce9ac923b829f88d7c10b9aacc2a298da21
rhuanpk <dev@rhuanpk.net>
2026-07-21 16:37:22 -0300

    feat: add new way to get path flag folder

--- .off/zsh/configure.sh
+++ .off/zsh/configure.sh
@@ -2,6 +2,7 @@
 
 url_setpath='https://raw.githubusercontent.com/rhuanpk/linux/main/scripts/.private/setpath.sh'
 [ -z "$PATH_CFGBKP" ] && source /etc/environment
+[ -z "$PATH_CFGBKP" ] && PATH_CFGBKP="$(find "$HOME/" -type f -name '.cfgbkp.pf' 2>&- \ | xargs dirname 2>&- \ | tail -1)"
 path_cfgbkp="${PATH_CFGBKP:-$(curl -fsL "$url_setpath" | bash -s -- -p cfgbkp)}"
 
 path_zsh="$path_cfgbkp/my-theme"
